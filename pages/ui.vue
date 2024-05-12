63d17e73d3d5a62900e88b4967c578a877ae6dd3
BiLLie WiLLie <vadimbelinovich@gmail.com>
2024-05-12 17:41:48 +0300

    add prettier

--- pages/ui.vue
+++ pages/ui.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1 class="~text-[20px]/[40px]"><span>первая страница</span></h1>
+  <section>
+    <div class="container">
+      <h1 class="~text-[20px]/[40px]">
+        <span>fluid text</span>
+      </h1>
+    </div>
+  </section>
+  <section>
+    <div class="container grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-4">
+      <div
+        v-for="i in 7"
+        :key="i"
+        class="border-2 p-2 rounded"
+      >
+        card {{ i }}
+      </div>
+    </div>
+  </section>
 </template>
