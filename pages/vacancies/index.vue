5445ddc74b6be9fe3fc349b503efaff2ba5ae531
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-05 19:33:16 +0300

    remove seo

--- pages/vacancies/index.vue
+++ pages/vacancies/index.vue
@@ -1,17 +1,17 @@
 <template>
 
-  <section class="mb-20 pt-14">
+  <section class="mb-16 pt-14">
     <div class="container">
       <h1 class="section-title">Вакансии</h1>
     </div>
   </section>
 
-<!--  <section class="mb-20">-->
-<!--    <div class="container">-->
-<!--      <h2 class="section-title mb-20">Основные направления кадровой политики «БиоЛайн»</h2>-->
-<!--      <p>text</p>-->
-<!--    </div>-->
-<!--  </section>-->
+  <section>
+    <div class="container">
+      <h2 class="section-title mb-20">Основные направления кадровой политики «БиоЛайн»</h2>
+      <p>text</p>
+    </div>
+  </section>
 
 <!--  <section class="mb-20">-->
 <!--    <div class="container">-->
