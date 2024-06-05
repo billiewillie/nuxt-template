84a840b16259aedcb6179e1c56c0278c1ecb789d
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-05 12:05:18 +0300

    add partners

---
+++ data/partners.ts
@@ -0,0 +1,20 @@
+const PARTNERS = [
+  {
+    id: 101,
+    title: 'Leica Microsystems',
+    slug: 'leica-microsystems',
+    image: '/img/partners/leica-microsystems.svg',
+    category: 'Биолайн',
+    link: 'https://www.leica-microsystems.com/'
+  },
+  {
+    id: 201,
+    title: 'Systec',
+    slug: 'systec',
+    image: '/img/partners/systec.svg',
+    category: 'БиоСистем',
+    link: 'https://www.systec-lab.com/'
+  }
+]
+
+export default PARTNERS
