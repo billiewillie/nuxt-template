56971cd11aa0f9ae2915277394d1ed3570f566e0
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-05 17:33:47 +0300

    edit vacancies

--- pages/vacancies/index.vue
+++ pages/vacancies/index.vue
@@ -1,14 +1,22 @@
 <template>
 
-  <section class="mb-16 pt-14">
+  <section class="mb-20 pt-14">
     <div class="container">
       <h1 class="section-title">Вакансии</h1>
     </div>
   </section>
 
-  <section class="h-screen">
+  <section class="mb-20">
     <div class="container">
+      <h2 class="section-title mb-20">Основные направления кадровой политики «БиоЛайн»</h2>
+      <p>text</p>
+    </div>
+  </section>
 
+  <section class="mb-20">
+    <div class="container">
+      <h2 class="section-title mb-20">Наша компания постоянно развивается и сотрудники развиваются вместе с ней</h2>
+      <p>text</p>
     </div>
   </section>
 
