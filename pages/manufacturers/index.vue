98fbab9d3adffe2994f92f302df259e3f18d5502
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-06 12:46:37 +0300

    edit manufacturers

--- pages/manufacturers/index.vue
+++ pages/manufacturers/index.vue
@@ -6,4 +6,32 @@
     </div>
   </section>
 
+  <section>
+    <div class="container grid grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-4 mb-16">
+      <NuxtLink
+        v-for="i in 9"
+        :key="i"
+        to="/">
+        <Card class="flex flex-col gap-4">
+          <CardHeader>
+            <NuxtImg
+              src="/img/partners/leica-microsystems.svg"
+              width="80"
+              class="mb-8"
+              alt="leica microsystems" />
+            <h2 class="font-bold">Leica Biosystems</h2>
+          </CardHeader>
+          <CardContent>
+            <p class="text-2xl">
+              <b>Leica Biosystems</b> - разработчик решений и средств автоматизации рабочих процессов для проведения
+              патогистологических исследований.
+            </p>
+          </CardContent>
+        </Card>
+      </NuxtLink>
+    </div>
+  </section>
+
 </template>
+<script setup>
+</script>
