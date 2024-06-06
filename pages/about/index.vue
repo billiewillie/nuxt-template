f411c4614b210f1b416c14df104cb11d892334a5
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-06 13:54:09 +0300

    edit support

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,40 @@
+<script setup>
+import CATEGORIES from '~/data/categories'
+import { Card, CardContent } from '~/components/ui/card'
+</script>
+
 <template>
 
   <section class="mb-16 pt-14">
     <div class="container">
       <h1 class="section-title">О компании</h1>
+    </div>
+  </section>
+
+  <section class="mb-40">
+    <div class="container">
+      <div class="grid grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-4">
+        <Card
+          v-for="(item, index) in 6"
+          :key="index"
+          class="flex flex-col gap-6 p-4 items-center text-center">
+
+          <CardHeader class="flex flex-row gap-8 items-center">
+            <NuxtImg
+              src="/img/categories/icons/1.svg"
+              class="w-[50px] object-contain object-center"
+              alt="alt" />
+            <CardTitle>Удобство работы с нами</CardTitle>
+          </CardHeader>
+          <Separator />
+          <CardContent class="p-0 font-medium">
+            <p class="text-xl">
+              Мы признательны всем, кто ежедневно делает свой вклад в сохранение здоровья людей,
+              мы восхищаемся вашим трудолюбием
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   </section>
 
--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,8 +1,34 @@
+<script setup>
+import { Card, CardContent } from '~/components/ui/card'
+</script>
+
 <template>
 
   <section class="mb-16 pt-14">
     <div class="container">
       <h1 class="section-title">Технический сервис</h1>
+    </div>
+  </section>
+
+  <section class="mb-16">
+    <div class="container">
+      <div class="grid grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-4">
+        <Card
+          v-for="(_,index) in 5"
+          :key="index"
+          class="flex flex-col gap-6 p-4 items-center text-center">
+          <NuxtImg
+            src="/img/categories/icons/1.svg"
+            class="w-[80px] object-contain object-center"
+            alt="alt" />
+          <CardContent class="p-0 font-medium">
+            <p>
+              Четкое планирование и прогнозирование профессиональной подготовки персонала и обеспечение современной
+              системы корпоративного обучения
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   </section>
 
