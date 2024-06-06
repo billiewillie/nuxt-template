77e03e0aba27f45636b545f132a8becc0d5b9b90
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-06 12:33:57 +0300

    edit in stock page

--- pages/in-stock/index.vue
+++ pages/in-stock/index.vue
@@ -18,13 +18,21 @@ import { Card, CardContent } from '~/components/ui/card'
           v-for="(_, index) in 4"
           :key="index"
           class="flex flex-col gap-6 p-4 items-center text-center">
-          <CardContent class="flex flex-col xl:flex-row w-full p-0">
-            <NuxtPicture
-              src="/img/items/item-1.jpg"
-              class="flex basis-1/3 object-contain object-center justify-center"
-              alt="alt" />
+          <CardContent class="flex flex-col xl:flex-row w-full p-0 gap-8">
+            <div class="flex basis-1/3 justify-center">
+              <BaseImage
+                class="w-[350]"
+                src="/img/items/item-1.jpg"
+                alt="alt"
+                aspect-ratio="aspect-square"
+                placeholder="bg-[#fff]"
+                width="350"
+                height="350"
+              />
+            </div>
+
             <div class="basis-2/3 flex flex-col items-start gap-6">
-              <h2>Автоматический разливочный модуль в чашки Петри</h2>
+              <h2 class="font-bold text-2xl">Автоматический разливочный модуль в чашки Петри</h2>
               <Separator />
               <p class="text-xl text-gray-500">
                 Производитель: Systec GmbH & Co. KG, Германия
@@ -48,7 +56,7 @@ import { Card, CardContent } from '~/components/ui/card'
                 height="24"
                 color="#575757"></Icon>
             </div>
-            <Button>Отправить запрос</Button>
+            <Button variant="outline">Отправить запрос</Button>
             <span>Перейти на страницу</span>
           </CardFooter>
         </Card>
