8b0abf16fa3c65429493fd9c38dbf3b0c5c62456
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-19 16:25:51 +0300

    edit seo

--- pages/news/[slug]/index.vue
+++ pages/news/[slug]/index.vue
@@ -36,7 +36,7 @@ useSeoMeta({
   ogImageWidth: 1200,
   ogImageHeight: 630,
   ogLocale: 'ru_RU',
-  twitterImage: () => banner.value,
+  twitterImage: () => banner.value ?? '/img/og-logo.jpg',
   twitterTitle: () => title.value,
   twitterDescription: () => description.value,
   twitterSite: 'bioline.vercel.app',
@@ -69,7 +69,7 @@ useHead({
     },
     {
       property: 'twitter:image',
-      content: () => banner.value
+      content: () => banner.value ?? '/img/og-logo.jpg'
     },
     {
       property: 'twitter:url',
