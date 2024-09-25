922ec3f18a7e91cee422294b5df6b95c279877dc
BiLLie WiLLie <vadimbelinovich@gmail.com>
2024-09-25 09:38:52 +0300

    edit sitemap

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -2,7 +2,5 @@ export default defineEventHandler(async (event) => {
   event.node.res.setHeader('Content-Type', 'text/xml')
   const { API_ENDPOINT } = useRuntimeConfig().public
 
-  const data = await $fetch(`${API_ENDPOINT}/sitemap/generate`)
-
-  return data
+  return await $fetch(`${API_ENDPOINT}/sitemap/generate`)
 })
